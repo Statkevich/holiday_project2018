941539d813ea28b9369ddaca86bd9d7039657f1a
Oksana <kursorksu777@gmail.com>
2018-07-18 23:08:12 +0300

    program

--- js/script.js
+++ js/script.js
@@ -20,4 +20,26 @@ $(function () {
         e.preventDefault();
         $('.slick-next').trigger('click');
     });
+    //========= this code to start slider================
+    $('.demands__slider__list').slick({
+        slidesToShow: 1,
+        dots: false,
+        infinite: true,
+        speed: 600,
+        autoplaySpeed: 3000,
+        autoplay: true,
+        pauseOnHover: true,
+        pauseOnDotsHover: true,
+        swipeToSlide: true
+    });
+    // ========= this code for customer button =====================
+    $('demands__text .slider__btn--l').on('click',function(e){
+        e.preventDefault();
+        $('demands__text .slick-prev').trigger('click');
+    });
+    $('demands__text .slider__btn--r').on('click',function(e){
+        e.preventDefault();
+        $('.demands__text .slick-next').trigger('click');
+    });
+
 });
